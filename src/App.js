660893136003dddd4235653fddfea01319228498
excellence-arch <oladipupomichael9@gmail.com>
2023-02-05 23:05:35 +0100

    API now working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState(false);
+  const url = `${process.env.REACT_APP_BASE_URL}/users`;
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    axios.post(url, {name, email}).then(res => {
+      console.log(res);
+    }).catch((err) => {
+      setError(err)
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <form onSubmit={(e) => submitForm(e)}>
+        <input type={'text'} value={name} onChange={(e) => setName(e.target.value)} required className="w-100 form-control" placeholder="Name"/>
+        <input type={'text'} value={email} onChange={(e) => setEmail(e.target.value)} className="w-100 form-control" placeholder="Email" />
+        <input type="submit" value="Submit" />
+    </form>
+    </>
+  )
 }
 
 export default App;
